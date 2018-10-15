14bcfea315532ffb90153a37c6f933c7f846e274
Piotr Walen <walen.piotr@gmail.com>
2018-10-15 23:50:41 +0200

    Add file size limit

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/jpeg"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -101,28 +102,45 @@ func trackHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	readLimit := int64(250e6)
+
 	id := ksuid.New().String()
 
 	dir := filepath.Join("data", "tmp", id)
 	err := os.Mkdir(dir, 0777)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	file, err := os.Create(filepath.Join(dir, "footage.mp4"))
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	n, err := io.Copy(file, r.Body)
+
+	data, err := ioutil.ReadAll(io.LimitReader(r.Body, readLimit))
+	if int64(len(data)) == readLimit {
+		http.Error(w, "File too big", http.StatusInternalServerError)
+		log.Fatal("File too big")
+		return
+	}
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+
+	}
+	err = ioutil.WriteFile(filepath.Join(dir, "footage.mp4"), data, 0777)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	j, err := json.Marshal(map[string]interface{}{
-		"id": id,
-		"n":  n,
+		"ID": id,
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
