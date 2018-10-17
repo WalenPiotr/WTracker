5cf2a4b7858ffa232559261fa38c3c6b3f3b52bb
Piotr Walen <walen.piotr@gmail.com>
2018-10-17 18:06:24 +0200

    Fix canvas size

--- client/src/components/RectSelect.tsx
+++ client/src/components/RectSelect.tsx
@@ -12,11 +12,6 @@ interface Point {
     y: number;
 }
 
-const canvasSize = {
-    x: 800,
-    y: 500,
-};
-
 interface RectSelectState {
     url: string;
     tracked: Rectangle;
@@ -99,16 +94,17 @@ class RectSelect extends React.Component<any, RectSelectState> {
         if (event.target instanceof HTMLCanvasElement) {
             const x0 = event.target.getBoundingClientRect().left;
             const y0 = event.target.getBoundingClientRect().top;
+            const x1 = event.target.getBoundingClientRect().right;
+            const y1 = event.target.getBoundingClientRect().bottom;
 
             const val = {
                 x: Math.round(
-                    ((event.clientX - x0) * this.state.size.x) / canvasSize.x,
+                    ((event.clientX - x0) * this.state.size.x) / (x1 - x0),
                 ),
                 y: Math.round(
-                    ((event.clientY - y0) * this.state.size.y) / canvasSize.y,
+                    ((event.clientY - y0) * this.state.size.y) / (y1 - y0),
                 ),
             };
-            console.log(val);
 
             this.setState((prevState: RectSelectState) => ({
                 ...prevState,
@@ -153,11 +149,9 @@ class RectSelect extends React.Component<any, RectSelectState> {
     };
 
     render() {
-        console.log(this.state);
         return (
             <div>
                 <Canvas
-                    size={canvasSize}
                     originalSize={this.state.size}
                     src={this.state.url}
                     onClick={this.handleClick}
@@ -171,16 +165,20 @@ class RectSelect extends React.Component<any, RectSelectState> {
                         >
                             1st Point
                         </SButton>
-                        <label>x = </label>
-                        <SInput
-                            value={this.state.tracked.min.x}
-                            onChange={this.handleChange('min', 'x')}
-                        />
-                        <label>y = </label>
-                        <SInput
-                            value={this.state.tracked.min.y}
-                            onChange={this.handleChange('min', 'y')}
-                        />
+                        <div>
+                            <SLabel>x = </SLabel>
+                            <SInput
+                                value={this.state.tracked.min.x}
+                                onChange={this.handleChange('min', 'x')}
+                            />
+                        </div>
+                        <div>
+                            <SLabel>y = </SLabel>
+                            <SInput
+                                value={this.state.tracked.min.y}
+                                onChange={this.handleChange('min', 'y')}
+                            />
+                        </div>
                     </SPointBox>
                     <SPointBox>
                         <SButton
@@ -189,16 +187,20 @@ class RectSelect extends React.Component<any, RectSelectState> {
                         >
                             2nd Point
                         </SButton>
-                        <label>x = </label>
-                        <SInput
-                            value={this.state.tracked.max.x}
-                            onChange={this.handleChange('max', 'x')}
-                        />
-                        <label>y = </label>
-                        <SInput
-                            value={this.state.tracked.max.y}
-                            onChange={this.handleChange('max', 'y')}
-                        />
+                        <div>
+                            <SLabel>x = </SLabel>
+                            <SInput
+                                value={this.state.tracked.max.x}
+                                onChange={this.handleChange('max', 'x')}
+                            />
+                        </div>
+                        <div>
+                            <SLabel>y = </SLabel>
+                            <SInput
+                                value={this.state.tracked.max.y}
+                                onChange={this.handleChange('max', 'y')}
+                            />
+                        </div>
                     </SPointBox>
                 </SControls>
             </div>
@@ -208,6 +210,8 @@ class RectSelect extends React.Component<any, RectSelectState> {
 
 const SPointBox = styled.div`
     margin: 10px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const SControls = styled.div`
@@ -219,6 +223,10 @@ const SControls = styled.div`
 
 const SInput = styled.input``;
 
+const SLabel = styled.label`
+    margin: 10px;
+`;
+
 const SButton = styled.button`
     background-color: ${({ highlight }) =>
         highlight ? 'green' : 'transparent'};
@@ -228,7 +236,6 @@ const SButton = styled.button`
 interface CanvasProps {
     src: string;
     onClick: (event: React.MouseEvent<HTMLCanvasElement>) => void;
-    size: Point;
     originalSize: Point;
     tracked: Rectangle;
 }
@@ -241,12 +248,19 @@ class Canvas extends React.Component<CanvasProps, any> {
         this.updateCanvas();
     }
 
-    drawRect(ctx: CanvasRenderingContext2D, tracked: Rectangle) {
-        const { originalSize, size } = this.props;
-        const x = (tracked.min.x / originalSize.x) * size.x;
-        const y = (tracked.min.y / originalSize.y) * size.y;
-        const w = ((tracked.max.x - tracked.min.x) / originalSize.x) * size.x;
-        const h = ((tracked.max.y - tracked.min.y) / originalSize.y) * size.y;
+    drawRect(
+        ctx: CanvasRenderingContext2D,
+        tracked: Rectangle,
+        originalSize: Point,
+    ) {
+        const x = Math.round((tracked.min.x * canvasSize.x) / originalSize.x);
+        const y = Math.round((tracked.min.y * canvasSize.y) / originalSize.y);
+        const w = Math.round(
+            ((tracked.max.x - tracked.min.x) * canvasSize.x) / originalSize.x,
+        );
+        const h = Math.round(
+            ((tracked.max.y - tracked.min.y) * canvasSize.y) / originalSize.y,
+        );
 
         ctx.fillStyle = 'rgba(255, 0, 0, 0.1)';
         ctx.fillRect(x, y, w, h);
@@ -279,28 +293,36 @@ class Canvas extends React.Component<CanvasProps, any> {
             const ctx = this.refs.canvas.getContext('2d');
             this.clearCanvas(ctx, this.refs.canvas);
             // await this.drawBackground(ctx, this.props.src);
-            this.drawRect(ctx, this.props.tracked);
+            this.drawRect(ctx, this.props.tracked, this.props.originalSize);
         }
     }
     render() {
         return (
-            <SDiv size={this.props.size}>
+            <SDiv size={canvasSize}>
                 <SCanvas
                     ref="canvas"
-                    width={this.props.size.x}
-                    height={this.props.size.y}
                     onClick={this.props.onClick}
+                    width={canvasSize.x}
+                    height={canvasSize.y}
+                    size={canvasSize}
                 />
-                <SImg src={this.props.src} size={this.props.size} />
+                <SImg src={this.props.src} size={canvasSize} />
             </SDiv>
         );
     }
 }
 
+const canvasSize = {
+    x: 1280,
+    y: 720,
+};
+
 const SCanvas = styled.canvas`
     position: absolute;
     z-index: 2;
     user-select: none;
+    width: ${({ size }: SImgProps) => `${size.x}px`};
+    height: ${({ size }: SImgProps) => `${size.y}px`};
 `;
 
 interface SImgProps {
@@ -310,14 +332,16 @@ interface SImgProps {
 const SImg = styled.img`
     width: ${({ size }: SImgProps) => `${size.x}px`};
     height: ${({ size }: SImgProps) => `${size.y}px`};
+
     position: absolute;
     z-index: 1;
     user-select: none;
 `;
 const SDiv = styled.div`
-    position: relative;
     width: ${({ size }: SImgProps) => `${size.x}px`};
     height: ${({ size }: SImgProps) => `${size.y}px`};
+
+    position: relative;
     margin: 10px auto;
     user-select: none;
 `;
