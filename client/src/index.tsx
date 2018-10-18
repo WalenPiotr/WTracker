d81a514b0e250a0b57a41808ab94322db706b7ee
Piotr Walen <walen.piotr@gmail.com>
2018-10-18 13:35:50 +0200

    Change import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import RectSelect from './components/RectSelect';
+import RectSelect from '@components/RectSelect/RectSelect';
 
 ReactDOM.render(<RectSelect />, document.getElementById('root'));
 registerServiceWorker();
